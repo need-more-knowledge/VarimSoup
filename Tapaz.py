730dc36df4b0fc1927ba5ecdb7972dcb9043af4d
DESKTOP-PLDT2J8\Сінабони <eushakov@provectus.com>
2023-10-16 15:45:45 +0300

    Saving data into the JSON

--- Tapaz.py
+++ Tapaz.py
@@ -24,39 +24,41 @@ url = "https://ru.tap.az";
 with open("tapaz.html", encoding="utf-8") as file:
     src = file.read()
 soup = BeautifulSoup(src, "lxml");
+#
+# #Electronics category name
+# cat_name = soup.find("div",class_="header-categories-row").find("a").find("span").text
+# print(cat_name)
+#
+# #Electronics href
+# elec_href = url + soup.find("div",class_="header-categories-row").find("a").get("href");
+# print(elec_href)
+#
+# # ALL Electronica obyavi
+# All_href = soup.find(class_="header-subcategories-columns").find("a",class_ = "primary-subcategories-i").get("href");
+# print (url + All_href)
+# All_tr_name = soup.find("span",class_ = "primary-subcategories-i_name").text;
+# print(All_tr_name)
+#
+# #Sub cat names + links
+# sub_cat_links = soup.find("div",class_="header-subcategories-columns")
+# for i in sub_cat_links:
+#     link = i.get("href");
+#     name = i.find("span","primary-subcategories-i_name").text;
+#     print(url + link)
+#     print(name)
 
-#Electronics category name
-cat_name = soup.find("div",class_="header-categories-row").find("a").find("span").text
-print(cat_name)
 
-#Electronics href
-elec_href = url + soup.find("div",class_="header-categories-row").find("a").get("href");
-print(elec_href)
-
-# ALL Electronica obyavi
-All_href = soup.find(class_="header-subcategories-columns").find("a",class_ = "primary-subcategories-i").get("href");
-print (url + All_href)
-All_tr_name = soup.find("span",class_ = "primary-subcategories-i_name").text;
-print(All_tr_name)
-
-#Sub cat names + links
-sub_cat_links = soup.find("div",class_="header-subcategories-columns")
-for i in sub_cat_links:
-    link = i.get("href");
-    name = i.find("span","primary-subcategories-i_name").text;
-    print(url + link)
-    print(name)
-
-
-# #Transport category name
-transCatName = soup.find("a","header-category transport").find("span").text;
-print(transCatName);
-
-#Transport category URL
-transCatUrl = url + soup.find("a","header-category transport").get("href");
-print(transCatUrl)
+# # #Transport category name
+# transCatName = soup.find("a","header-category transport").find("span").text;
+# print(transCatName);
+#
+# #Transport category URL
+# transCatUrl = url + soup.find("a","header-category transport").get("href");
+# print(transCatUrl)
 
 # ALL Categories1
+allCatDict ={};
+
 allCatList1 = soup.find(class_="header-navigation").find(class_="l-center").find("div");
 for i in allCatList1:
     link = i.get("href");
@@ -72,6 +74,9 @@ for i in allCatList1:
     if name:
         print(name);
 
+    allCatDict[name] = link;
+
+   # print(f"{name}:{link}"); - variant 3apisi printa c obyeden. 2-x peremennix
 
 
 
@@ -91,8 +96,13 @@ for i in allCatList2:
     if name == "Магазины":
         print(name);
         break;
+    if name == "Все объявления":
+        pass;
     if name:
         print (name);
+fullLink = url + link;
+allCatDict[name] = fullLink;
 
 
-
+with open ("allCatDict.json","w",encoding="utf-8") as file:
+    json.dump(allCatDict, file, indent=4, ensure_ascii=False);
