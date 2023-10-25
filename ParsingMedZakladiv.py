4f00ad8d949bf50a664c3062f4a5bcb606a0a76c
DESKTOP-PLDT2J8\Сінабони <evgeniy.ushakov@gmail.com>
2023-10-25 18:32:39 +0300

    Added: Finished medzaklad.py

--- ParsingMedZakladiv.py
+++ ParsingMedZakladiv.py
@@ -8,6 +8,7 @@ from urllib.parse import unquote
 import random
 import json
 from selenium.webdriver.common.by import By
+from urllib.parse import unquote
 
 
 headers = {
@@ -44,113 +45,117 @@ headers = {
 
 file_path = 'Source-page.html'
 
-def get_items_urls(file_path):
-    with open(file_path, encoding ='UTF-8') as file:
-        src = file.read()
+
+#Збираємо всі посилання на клініки і зберігаємо в текстовик
+# def get_items_urls(file_path):
+#     with open(file_path, encoding ='UTF-8') as file:
+#         src = file.read()
+#
+#     soup = BeautifulSoup(src, "lxml")
+#     items_divs = soup.find_all("div", class_="minicard-item__info")
+#
+#     urls = []
+#     for item in items_divs:
+#         item_url = item.find("h2",class_="minicard-item__title").find("a").get("href");
+#         print(item_url)
+#         urls.append(item_url)
+#
+#     with open("UrlList.txt", "w") as file:
+#         for url in urls:
+#             file.write(f"{url}\n")
+#
+#     return "[INFO] Urls collected successfully!"
 
 
+# Пишимо функцію яка вичитає тектовик и збере список вичещених від пробелів посилань
+file_path2 = "UrlList.txt";
+def get_data(file_path2):
+    with open(file_path2) as file:
+        # urls_list = file.readlines()
+        #
+        # clear_urls_list = []
+        # for url in urls_list:
+        #     url = url.strip()
+        #     clear_urls_list.append(url)
+        # print(clear_urls_list)
+        # Тут написано то саме, що і в коді з 73 по 79 лінії, але коротше і краще
+        urls_list = [url.strip() for url in file.readlines()]
 
-    soup = BeautifulSoup(src, "lxml")
-    items_divs = soup.find_all("div", class_="minicard-item__info")
-    print(items_divs.count(1))
+# Додаєм хедери і юзерагент і в циклі пробігаємо по кожному посиланні і збираємо данні
+    result_list = [] #все що знайшли - наповнюєм сюди
+    urls_count = len(urls_list)
+    count = 1
+    for url in urls_list:
+        response = requests.get(url=url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")
 
-    # urls = []
-    # for item in items_divs:
-    #     item_url = item.find("h2",class_="minicard-item__title".find("a").get("href"));
-    #     print(item_url)
-    #     urls.append(item_url)
-    #
-    # with open("UrlList.txt", "w") as file:
-    #     for url in urls:
-    #         file.write(f"{url}\n")
-    #
-    # return "[INFO] Urls collected successfully!"
+        try:
+            item_name = soup.find("span", {"itemprop": "name"}).string.strip()
+        except Exception as _ex:
+            item_name = None
 
-#
-# def get_data(file_path):
-#     with open(file_path) as file:
-#         # urls_list = file.readlines()
-#
-#         # clear_urls_list = []
-#         # for url in urls_list:
-#         #     url = url.strip()
-#         #     clear_urls_list.append(url)
-#         # print(clear_urls_list)
-#
-#         urls_list = [url.strip() for url in file.readlines()]
-#
-#     result_list = []
-#     urls_count = len(urls_list)
-#     count = 1
-#     for url in urls_list:
-#         response = requests.get(url=url, headers=headers)
-#         soup = BeautifulSoup(response.text, "lxml")
-#
-#         try:
-#             item_name = soup.find("span", {"itemprop": "name"}).text.strip()
-#         except Exception as _ex:
-#             item_name = None
-#
-#         item_phones_list = []
-#         try:
-#             item_phones = soup.find("div", class_="service-phones-list").find_all("a", class_="js-phone-number")
-#
-#             for phone in item_phones:
-#                 item_phone = phone.get("href").split(":")[-1].strip()
-#                 item_phones_list.append(item_phone)
-#         except Exception as _ex:
-#             item_phones_list = None
-#
-#         try:
-#             item_address = soup.find("address", class_="iblock").text.strip()
-#         except Exception as _ex:
-#             item_address = None
-#
-#         try:
-#             item_site = soup.find(text=re.compile("Сайт|Официальный сайт")).find_next().text.strip()
-#         except Exception as _ex:
-#             item_site = None
-#
-#         social_networks_list = []
-#         try:
-#             item_social_networks = soup.find(text=re.compile("Страница в соцсетях")).find_next().find_all("a")
-#             for sn in item_social_networks:
-#                 sn_url = sn.get("href")
-#                 sn_url = unquote(sn_url.split("?to=")[1].split("&")[0])
-#                 social_networks_list.append(sn_url)
-#         except Exception as _ex:
-#             social_networks_list = None
-#
-#         result_list.append(
-#             {
-#                 "item_name": item_name,
-#                 "item_url": url,
-#                 "item_phones_list": item_phones_list,
-#                 "item_address": item_address,
-#                 "item_site": item_site,
-#                 "social_networks_list": social_networks_list
-#             }
-#         )
-#
-#         time.sleep(random.randrange(2, 5))
-#
-#         if count%10 == 0:
-#             time.sleep(random.randrange(5, 9))
-#
-#         print(f"[+] Processed: {count}/{urls_count}")
-#
-#         count += 1
-#
-#     with open("lesson12/result.json", "w") as file:
-#         json.dump(result_list, file, indent=4, ensure_ascii=False)
-#
-#     return "[INFO] Data collected successfully!"
+        item_phones_list = []
+        try:
+            item_phones = soup.find("div", class_="service-phones-list").find_all("a", class_="js-phone-number")
+            # забираємо телефон повністю "tel:+79813361632" і розділяємо по : забираючи праву частину із номером
+            for phone in item_phones:
+                item_phone = phone.get("href").split(":")[-1].strip()
+                item_phones_list.append(item_phone)
+        except Exception as _ex:
+             item_phones_list = None
+
+        try:
+           item_address = soup.find("address", class_="iblock").text.strip()
+        except Exception as _ex:
+            item_address = None
+
+        try:
+            item_site = soup.find(text=re.compile("Сайт|Официальный сайт")).find_next().text.strip()
+        except Exception as _ex:
+            item_site = None
+
+        social_networks_list = []
+        try:
+            item_social_networks = soup.find("div",class_="js-service-socials").find('div',class_="z-text--13").find_all("a");
+
+            for sn in item_social_networks:
+                sn_url = sn.get("href")
+                sn_url = unquote(sn_url.split("?to=")[1].split("&")[0])
+                #Наповнюєм список чистим урлом
+                social_networks_list.append(sn_url)
+        except Exception as _ex:
+            social_networks_list = None
+
+        result_list.append(
+            {
+                "item_name": item_name,
+                "item_url": url,
+                "item_phones_list": item_phones_list,
+                "item_address": item_address,
+                "item_site": item_site,
+                "social_networks_list": social_networks_list
+            }
+        )
+
+        time.sleep(random.randrange(2, 5))
+
+        if count%10 == 0:
+            time.sleep(random.randrange(3, 6))
+
+        print(f"[+] Processed: {count}/{urls_count}")
+
+        count += 1
+
+    with open("MedClinResult.json", "w",encoding = 'UTF-8') as file:
+        json.dump(result_list, file, indent=4, ensure_ascii=False)
+
+    return "[INFO] Data collected successfully!"
 
 
 def main():
     #get_source_html(url="https://spb.zoon.ru/medical/?search_query_form=1&m%5B5200e522a0f302f066000055%5D=1&center%5B%5D=59.91878264665887&center%5B%5D=30.342586983263384&zoom=10")
-    print(get_items_urls(file_path))
-    # print(get_data(file_path="file_path"))
+    #print(get_items_urls(file_path))
+    print(get_data(file_path2))
 
 
 if __name__ == "__main__":
